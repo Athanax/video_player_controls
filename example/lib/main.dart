b5e1ac6a30a0a415e29436077678bc671efed9bb
Zeed <athanaxwambua@gmail.com>
2020-08-17 15:01:53 +0300

    add list of subtitles

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -118,13 +118,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: VideoPlayerControls(
-          controller: controller,
-        ),
-      ),
+    return VideoPlayerControls(
+      controller: controller,
     );
   }
 }
